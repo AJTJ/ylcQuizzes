a210c81def49d65a141ac8cc5ed8c9779d9c4cff
Aaron Janke <janke.aaron@gmail.com>
2019-01-01 23:49:38 -0500

    update

---
+++ precache-manifest.618b630a17be1b5c1afd9974aed58eed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fd7ece186c4387ae1777852de0142ac",
+    "url": "/ylcQuizzes/static/media/dark_flake.5fd7ece1.png"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "/ylcQuizzes/static/media/baseline-menu-24px.c275e518.svg"
+  },
+  {
+    "revision": "ec8c45d95d0225a597ea",
+    "url": "/ylcQuizzes/static/js/runtime~main.ec8c45d9.js"
+  },
+  {
+    "revision": "5569ef4697013cb0ad78",
+    "url": "/ylcQuizzes/static/js/main.5569ef46.chunk.js"
+  },
+  {
+    "revision": "06020d20649825352d69",
+    "url": "/ylcQuizzes/static/js/1.06020d20.chunk.js"
+  },
+  {
+    "revision": "5569ef4697013cb0ad78",
+    "url": "/ylcQuizzes/static/css/main.c87d3b52.chunk.css"
+  },
+  {
+    "revision": "06020d20649825352d69",
+    "url": "/ylcQuizzes/static/css/1.0a5936d0.chunk.css"
+  },
+  {
+    "revision": "ff7ed63137b79c924f207f565bb1ed1c",
+    "url": "/ylcQuizzes/index.html"
+  }
+];
